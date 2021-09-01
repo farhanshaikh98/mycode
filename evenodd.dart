8ae3459b04e49eee6da516bdf40d85a911f5e365
farhanshaikh98 <89948560+farhanshaikh98@users.noreply.github.com>
2021-09-01 16:45:03 -0700

    Add files via upload

---
+++ evenodd.dart
@@ -0,0 +1,14 @@
+void main(){
+  
+ int  n=50;
+ 
+  for(int i = 1; i <=n; i++)
+  {
+    if(i%2==0){
+      print("$i number is even");
+    }
+    else{
+      print("$i number is odd");
+    }
+  }
+}
