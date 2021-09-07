8110c5db917ec5b269d850eebbaa851cf530df3a
farhanshaikh98 <89948560+farhanshaikh98@users.noreply.github.com>
2021-09-08 03:51:38 +0530

    Add files via upload

---
+++ loop.dart
@@ -0,0 +1,39 @@
+void main()
+{
+
+  
+ 
+     int n=2; 
+  for(int i=1; i<=10; i++)           //table of 2 number  
+  {
+    int  temp=n*i;                 
+    print('2 * $i = $temp');         
+  }
+  
+   for(var i=2; i<=50; i++)            // 100 odd number print
+  {
+    if(i%2!=0)                     
+      print('$i');               
+    
+  }
+ 
+  for(var i=2; i<=100; i++)            // 100 even number print
+  {
+    if(i%2==0)                     
+      print('$i');               
+    
+  }
+  
+  
+  
+  var n=5;                 
+  var fact=1;              
+  for(int i=1; i<=n; i++)           //  factorial number
+  {
+    fact=fact*i;               
+  }
+  print('factorial of $n= $fact');         
+
+
+ 
+  
