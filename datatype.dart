dd7b859a31716ad527bf68042743b30417cbc44b
farhanshaikh98 <89948560+farhanshaikh98@users.noreply.github.com>
2021-09-08 03:19:29 +0530

    Add files via upload

---
+++ datatype.dart
@@ -0,0 +1,61 @@
+void main() {
+  
+    // int value
+  
+  int a=25;                   //integer assigment
+  print(a);                    
+  print(a.runtimeType);         
+ 
+   double  value
+   double  n=100.45;
+   print double value.
+   print(n);
+   print(n.runtimeType);
+  
+      string name 
+        
+  String name='farhan shaikh';  //string initialze
+  print(name);                  // print string
+  print(name.runtimeType);      // check its data type.
+    
+    //boolean 
+    bool name = true;           // bool initialize
+    print(name);                // print the bool value
+    print(name.runtimeType);     // check its date types.
+ 
+  bool surname =false;     
+  print(surname);        
+  print(surname.runtimeType);
+
+  
+    // VAR 
+ var name='farhan shaikh';   // name intilize using var types
+  print(name);                 // print var
+  print(name.runtimeType);     // var type
+  
+  var age=23;                  // same as above 
+  print(age);
+  print(age.runtimeType);
+  
+  var percentage=66.45;
+  print(percentage);
+  print(percentage.runtimeType);
+ 
+  
+  // dynamic
+  
+   dynamic name='farhan shaikh';       //dynamic value intialize
+  print(name);                          //print dynamic 
+  print(name.runtimeType);               //check its date type
+ 
+  // number data types
+   num age=10;                         // num value intialize
+  print(age);                            // print age
+  print(age.runtimeType);               // check its type
+  
+  num s1=154.45;              //same is above
+  print(s1);
+  print(s1.runtimeType);
+  
+  
+}
