4f65cc232e57691b0700957dfdf979d2e4c30a9a
farhanshaikh98 <89948560+farhanshaikh98@users.noreply.github.com>
2021-09-08 03:30:15 +0530

    Add files via upload

---
+++ conditionoperators.dart
@@ -0,0 +1,80 @@
+void main()
+{
+  // arthematic operators
+/*   
+  var a=50;    //intialize the integer value
+  var b=36;
+
+  // print the all operators perfomance 
+ print(a+b);    
+ print(a*b);   
+ print(a-b);    
+ print(a/b);   
+ print(a~/b)   
+ print(a%=b);  
+*/
+
+ 
+
+ /* 
+  int i=5;             // assign the value of i
+                  //  increment or decrement operation
+  i++;
+  print(i);//6
+  print(--i);//pre decrement
+  print(++i);//pre increment
+  print(i--); //post decrement
+  print(i);    //5
+  i+=6;     // increment 6
+  print(i);  //11
+ */
+
+/*  // comparison or relational operators.
+    // assign the value of a b c d;
+   var a=70;
+  var b=50;
+  var c=50;
+  var d=100;
+
+   print(a>b);
+  print(d>c);
+  print(d<=c);
+  print(a<b);
+  print(a>=b);
+ */
+
+/*  // equality operators
+    var x=10;
+    var y=10;
+    var z=20;
+
+  print(x==y); // true
+  print(x!=y); //false
+  print(z!=y); // true
+  print(z==10); // fakse
+*/
+
+
+  // logical operators
+  dynamic a=10;
+  dynamic b=20;
+  dynamic c=10;
+  dynamic d=100;
+
+
+  if(a==b && a==c)   //check condition
+  {
+    print(true);       // in this both condition are true
+}
+  else
+  {
+    print("condition is not satisfy");    
+  }
+ 
+
+  if(a==b || a!=b)
+  {
+    print("one condition will satisfy"); // conclusion one condtion is true condition will true
+  }
+
+ } 
